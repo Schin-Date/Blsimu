b55deebf54d81e5739b24d74b626ffb708514bfd
Schin Date <schin@spring8.or.jp>
2016-08-26 16:06:17 +0900

    Unnecessary files removed

--- garb/Witpy/include/witpy.h
+++
@@ -1,9 +0,0 @@
-// If you read this line, the file is originally a copy from 
-#ifndef __WITPY_H
-#define __WITPY_H
-#include "SetDoPrm.h"
-namespace witpy {
-    static const int C_STR_LEN_MAX = 256;
-    //131122 used in galeo::RtTree
-}// namespace witpy
-#endif
